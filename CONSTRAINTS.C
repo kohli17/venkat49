05e0adbda3756fe23f18c70cf33df14af4af5ea8
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-05 16:08:48 +0530

    Create CONSTRAINTS.C
    PRINT NUMBER

---
+++ CONSTRAINTS.C
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+int main()
+{
+    int a[50], i, j,n,temp;
+    printf(" enter n"); 
+    scanf("%d", &n);
+printf("enter array ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+     
+        for(j=i+1; j<n; j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+  for(i=0; i<n; i++)
+    {
+        printf("\n%d\t", a[i]);
+    }
+
+    return 0;
+
+}
+
