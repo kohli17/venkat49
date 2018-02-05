e1a9fd71b942a605713cc043389551d04a6ef5a5
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-05 16:43:24 +0530

    Create FIBONACCI.C
    print fibonacci series

---
+++ FIBONACCI.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int first=0,second=1,temp,n,i;
+printf("Enter the range");
+scanf("%d",&n);
+printf("%d %d",first,second);
+for(i=1;i<n;i++)
+{
+temp=first+second;
+first=second;
+second=temp;
+printf(" %d",second);
+}
+return 0;
+}
