67c1a701d881669c1728c9f01f41b1faea9c9032
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-05 16:29:45 +0530

    Create SWAP.C
    print swap number

---
+++ SWAP.C
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+ int main()
+{
+    int a=5,b=8,t;
+    printf("value of 'a' and 'b' before swaping%d%d",a,b);
+    t=a;
+    a=b;
+    b=t;
+    printf("value of 'a' and 'b' after swaping%d%d",a,b);
+    return 0;
+    }
