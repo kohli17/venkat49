7673845d528990f98f1753187f1f28101c77e8bf
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-05 16:05:22 +0530

    Create ABS.C
    PRINT TIME

---
+++ ABS.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c,d,e,f;
+printf("give the 1st time");
+scanf("%d %d",&a,&b);
+printf("give the 2nd time");
+scanf("%d %d",&c,&d);
+e=a-c;
+f=b-d;
+printf("%d %d",e,f);
+return 0;
+}
+© 2018 GitHub, Inc.
